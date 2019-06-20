9184f8b146e1c30a444490f66c461c09b1aa70d2
yuhaibo <yuhaibo@yuhaibodeMacBook-Pro.local>
2019-06-21 00:25:46 +0800

    add develop tools

---
+++ src/intro.c
@@ -0,0 +1,17 @@
+//intro
+
+#include <stdio.h>
+
+//test include text
+#include "inctest.txt"
+
+// extern int var_int;
+
+int
+main()
+{
+    printf("Hello World!\n");
+    printf("var_int=%d \n", var_int);
+    
+    return 0;
+}
